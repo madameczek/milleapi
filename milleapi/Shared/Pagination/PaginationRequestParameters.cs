785a4e859c731bd38af83de1c145f9f87af4e104
Marek <marek@adameczek.pl>
2024-06-16 08:48:04 +0200

    Update xml doc for swagger use.

--- milleapi/Shared/Pagination/PaginationRequestParameters.cs
+++ milleapi/Shared/Pagination/PaginationRequestParameters.cs
@@ -11,7 +11,7 @@ public class PaginationRequestParameters
     public int PageNumber { get; set; }
 
     /// <summary>
-    /// Optional, [Range(1, 100)] 
+    /// Optional, [Range(1, 100)], default = 25
     /// </summary>
     [Range(1, 100)] 
     public int PageSize { get; set; } = 25;
